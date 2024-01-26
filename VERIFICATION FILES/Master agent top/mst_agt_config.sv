5bb705d608bca89bbff1350649bc0b6fa6899601
vishnupriyan S <89830986+Vishnutpgit@users.noreply.github.com>
2024-01-26 11:33:16 +0530

    Create mst_agt_config.sv

---
+++ VERIFICATION FILES/Master agent top/mst_agt_config.sv
@@ -0,0 +1,21 @@
+class mst_agt_config extends uvm_object ;
+
+ `uvm_object_utils(mst_agt_config)
+
+virtual AHB_if mvif ;
+
+uvm_active_passive_enum is_active = UVM_ACTIVE;
+
+// Declare the mon_rcvd_xtn_cnt as static int and initialize it to zero
+static int mon_rcvd_xtn_cnt = 0;
+
+// Declare the drv_data_sent_cnt as static int and initialize it to zero
+static int drv_data_sent_cnt = 0;
+
+
+function new(string name = "mst_agt_config");
+  super.new(name);
+endfunction
+
+
+endclass
